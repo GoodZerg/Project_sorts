4b7b1669819e3ca130112e9ab5d1a89165de45ca
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2019-12-03 22:26:31 +0300

    commit bubble_sort_slow

--- Sorts.h
+++ Sorts.h
@@ -18,7 +18,7 @@ class Sorts {
     static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
   }
-  std::string print_vector( std::vector<int>& arr) {
+  std::string print_vector(std::vector<int>& arr) {
     std::stringstream ss;
     std::string s;
     for (int i = 0; i < arr.size() - 1; ++i) {
@@ -29,13 +29,38 @@ class Sorts {
     getline(ss, s);
     return s;
   }
+  data bubble_sort_slow(std::vector<int>& arr) {
+    data return_data;
+    int tmp = 0;
+    return_data.operations += 1;
+    return_data.operations += 1;
+    for (int i = 0; i < arr.size(); i++) {
+      return_data.comparisons += 1;
+      return_data.operations += 1;
+      return_data.operations += 1;
+      for (int j = arr.size() - 1; j > 0; j--) {
+        return_data.operations += 1;
+        return_data.comparisons += 1;
+        if (arr[j] < arr[j - 1]) {
+          return_data.comparisons += 1;
+          std::swap(arr[j], arr[j - 1]);
+          return_data.operations += 3;
+        }
+      }
+    }
+
+    return return_data;
+  }
   data bubble_sort_fast(std::vector<int>& arr) {
     data return_data;
     bool b = true;
+    return_data.operations += 1;
+    return_data.operations += 1;
     for (int i = 0; i < arr.size(); i++) {
       b = true;
       return_data.operations += 2;
       return_data.comparisons += 1;
+      return_data.operations += 1;
       for (int j = arr.size() - 1; j >= (i + 1); j--) {
         return_data.operations += 1;
         return_data.comparisons += 1;
@@ -46,8 +71,11 @@ class Sorts {
           return_data.operations += 4;
         }
       }
-      if (b) break;
-      return_data.comparisons += 1;
+      if (b){
+        return_data.comparisons += 1;
+        break;
+      }
+     
     }
     return return_data;
   }
